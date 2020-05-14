27545c00d2380354e39e8d107d6dc120b886fd77
Amano0 <60700409+Amano0@users.noreply.github.com>
2020-05-14 11:50:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,25 +1,25 @@
 import os
 
 import discord
-client=discord.Client()
-token="put your bots token"
+client = discord.Client()
+token = "put your bots token"
 
 
 @client.event
 async def on_message(message):
 
     # userのデータが書かれているtextファイルのpath (ファイル名はuser.id)
-    USER_DATA_FILE_PATH=f'/MemberData/{message.author.id}.txt'
+    USER_DATA_FILE_PATH = f'/MemberData/{message.author.id}.txt'
 
     # messageの文字数
-    mozi_num=len(list(message.content))
+    mozi_num = len(list(message.content))
 
     # userの初期データとして新規userのファイルに書き込む内容
-    userdatalist=[
+    userdatalist = [
         message.author.display_name,
-        "0",  # 総メッセージ文字数
-        "0",  # 文字数を消費してLvを上げた後に残った文字数
-        "1",  # 現在のLv (初期から1Lvは有る)
+        mozi_num,  # 総メッセージ文字数
+        mozi_num,  # 文字数を消費してLvを上げた後に残った文字数
+        1,  # 現在のLv (初期から1Lvは有る)
     ]
 
     # 自分は拒否
@@ -32,26 +32,26 @@ async def on_message(message):
             with open(USER_DATA_FILE_PATH, mode="r") as f:
 
                 # txt内の文字列を行分けにして data_list というリストにする
-                data_list=f.readlines()
+                data_list = f.readlines()
 
                 # 読み込んだデータをもとに再度更新するために書き込みモードで読み込む
                 with open(USER_DATA_FILE_PATH, mode="w") as f:
                     # そのままでは\nがあるため、\nをforで回して消す
-                    data_list=[i.replace('\n','') for i in data_list]
+                    data_list = [i.replace('\n','') for i in data_list]
                     # 今までの発言の総文字数に今回の発言の文字数を加える (実際このデータいらないかも)
-                    data_list[1]=int(data_list[1]) + mozi_num
+                    data_list[1] = int(data_list[1]) + mozi_num
                     # 前回のLvUPで消費した残りの文字数に今回の発言の文字数を加える
-                    data_list[2]=int(data_list[2]) + mozi_num
+                    data_list[2] = int(data_list[2]) + mozi_num
 
                     #残りの文字数がLvUPに必要な文字数を超えたかどうか
                     if int(data_list[3])*200<=data_list[2]:  # 1LvUPに必要な文字数は 現在のレベル*200
 
                         # 必要量を下回るまでLvをあげる
-                        while int(data_list[3])*200<=data_list[2]:
+                        while int(data_list[3])*200 <= data_list[2]:
                             # 残りの文字数から必要量を引く
-                            data_list[2]-=int(data_list[3])*200
+                            data_list[2] -= int(data_list[3])* 200
                             # 引いたので、Lvを1上げる
-                            data_list[3]=int(data_list[3])+1
+                            data_list[3] = int(data_list[3])+ 1
 
                         text=("おめでとう！\n{message.author.mention}のレベルが **__{data_list[3]}__** に上がったよ！")
                         # LvUPしたらメッセージを送信
@@ -76,7 +76,21 @@ async def on_message(message):
 
                 # 書き込みモードで開く
                 with open(USER_DATA_FILE_PATH, mode="w") as f:
+                    if int(data_list[3])*200<=data_list[2]:  # 1LvUPに必要な文字数は 現在のレベル*200
+
+                        # 必要量を下回るまでLvをあげる
+                        while userdatalist[3]*200 <= userdatalist[2]:
+                            # 残りの文字数から必要量を引く
+                            userdatalist[2] -= userdatalist[3]*200
+                            # 引いたので、Lvを1上げる
+                            data_list[3] = userdatalist[3] + 1
+
+                        text=("おめでとう！\n{message.author.mention}のレベルが **__{data_list[3]}__** に上がったよ！")
+                        # 確定Lv1以上なのでメッセージ送信
+                        await message.channel.send(text)
+
                     # 初期データを書き込む
+                    userdatalist=[str(i) for i in userdatalist]
                     f.write('\n'.join(userdatalist))
 
 client.run(token)
